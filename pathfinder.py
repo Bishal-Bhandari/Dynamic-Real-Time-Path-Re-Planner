0d5591ad083cbc02a72f94798f619e21d44f88f9
bishal <bishalbhaandari62@gmail.com>
2025-11-14 10:24:56 +0100

    -Some Logic to pathfinder added

--- pathfinder.py
+++ pathfinder.py
@@ -4,3 +4,13 @@ import numpy as np
 class Pathfinder:
     def __init__(self, config):
         self.config = config
+
+    def find_path(self, start, goal, obstacles):
+        # Simple A* (replace later with D* Lite for dynamic replanning)
+        width = self.config["grid"]["width"]
+        height = self.config["grid"]["height"]
+
+        open_list = []
+        heapq.heappush(open_list, (0, start))
+        came_from = {}
+        g_score = {start: 0}
